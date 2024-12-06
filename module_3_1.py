26e0f1f0352c446f4cb195d4a85d4f4ce4e5922b
VeeL <153204649+Volvikveel@users.noreply.github.com>
2024-12-06 15:20:11 +0300

    Add files via upload
    module_3_2.py

--- module_3_1.py
+++ module_3_1.py
@@ -1,24 +1,16 @@
-# Функция count_calls подсчитывающая вызовы остальных функций
-calls = 0
+# Создайте функцию send_email, которая принимает 2 обычных аргумента
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    if "@" not in sender or not sender.endswith((".com", ".ru", ".net")):
+        print(f"Невозможно отправить письмо с адреса <{sender}> на адрес <{recipient}>.")
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса <{sender}> на адрес <{recipient}>.")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <{sender}> на адрес <{recipient}>.")
 
-def count_calls():
-    global calls
-    calls += 1
-
-def string_info(string):
-    count_calls()  # Увеличиваем счетчик вызовов
-    return (len(string), string.upper(), string.lower())
-
-def is_contains(string, list_to_search):
-    count_calls()  # Увеличиваем счетчик вызовов
-    string_lower = string.lower()
-    return any(string_lower in item.lower() for item in list_to_search)
-
-# Данные на вход
-print(string_info('Capybara'))
-print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic']))    # No matches
-
-# Проверяем количество вызовов, вызвав функцию
-print(calls)
+# Проверка
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
